6b7ad545d425126a5524e6ab8964e2ec9398f904
Torpus <torpus@hotmail.com>
2020-02-29 11:25:03 -0600

    removed unused cpu stuff

--- Torpus/breadsticks/src/main.rs
+++ Torpus/breadsticks/src/main.rs
@@ -1,10 +1,10 @@
 use std::env::args;
 
 mod get_breadsticks;
-mod get_cpus;
+// mod get_cpus;
 
 fn main() {
     let filename = args().nth(1).unwrap();
-    println!("Available cpus: {}", get_cpus::get());
+//    println!("Available cpus: {}", get_cpus::get());
     println!("{}", get_breadsticks::get(&filename));
 }
