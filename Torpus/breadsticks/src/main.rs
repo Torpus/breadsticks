ea4db00c42d8c86fe380008cbdd01c63b239f3df
Torpus <torpus@hotmail.com>
2020-03-01 00:09:04 -0600

    cleaned up commented code

--- Torpus/breadsticks/src/main.rs
+++ Torpus/breadsticks/src/main.rs
@@ -1,10 +1,8 @@
 use std::env::args;
 
 mod get_breadsticks;
-// mod get_cpus;
 
 fn main() {
     let filename = args().nth(1).unwrap();
-//    println!("Available cpus: {}", get_cpus::get());
     println!("{}", get_breadsticks::get(&filename));
 }
