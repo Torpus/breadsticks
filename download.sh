18ab7ca1f7b4b134ce24ce9f1b513635b256ec6d
Caleb Lemoine <caleblemoine@gmail.com>
2020-03-21 18:07:10 -0500

    test env vars

--- download.sh
+++ download.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 REMOTE_IP="142.93.124.58"
-DIR="breadsticks_test_files"
-BENCHMARK_FILE="randoms-1M.txt"
-# export for child processes to get path dynamically
-export BENCHMARK_FILE_PATH="${DIR}/${BENCHMARK_FILE}"
+BENCHMARK_FILE_DIR=${BENCHMARK_FILE_DIR:-"breadsticks_test_files"}
+BENCHMARK_FILE=${BENCHMARK_FILE:-"randoms-1M.txt"}
+BENCHMARK_FILE_PATH="${BENCHMARK_FILE_DIR}/${BENCHMARK_FILE}"
 
-[ -d $DIR ] || echo "${DIR} doesnt exist. Creating..." && mkdir $DIR
+[ -d $DIR ] || echo "${BENCHMARK_FILE_DIR} doesnt exist. Creating..." && mkdir $DIR
 
 [ -f "$BENCHMARK_FILE_PATH" ] || echo "${BENCHMARK_FILE_PATH} doesnt exist. Downloading..." && wget -O "$BENCHMARK_FILE_PATH" "http://${REMOTE_IP}/${BENCHMARK_FILE}"
