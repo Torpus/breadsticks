fb2ddfa3ae1b195ccff3d356ade52fe04e9ae597
Caleb Lemoine <caleblemoine@gmail.com>
2020-03-21 17:47:07 -0500

    add debug messages

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 REMOTE_IP="142.93.124.58"
-DIR="breadsticks"
+DIR="breadsticks_test_files"
 BENCHMARK_FILE="randoms-1M.txt"
 # export for child processes to get path dynamically
 export BENCHMARK_FILE_PATH="${DIR}/${BENCHMARK_FILE}"
 
-[ -d $DIR ] || mkdir $DIR
+[ -d $DIR ] || echo "${DIR} doesnt exist. Creating..." && mkdir $DIR
 
-[ -f "$BENCHMARK_FILE_PATH" ] || wget -O "$BENCHMARK_FILE_PATH" "http://${REMOTE_IP}/${BENCHMARK_FILE}"
+[ -f "$BENCHMARK_FILE_PATH" ] || echo "${BENCHMARK_FILE_PATH} doesnt exist. Downloading..." && wget -O "$BENCHMARK_FILE_PATH" "http://${REMOTE_IP}/${BENCHMARK_FILE}"
